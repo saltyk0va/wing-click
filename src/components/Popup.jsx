ff6ff9fc06084544155fc604c62f3b0073ea7c47
Stefka Krause <stef.krausemoncada@gmail.com>
2025-01-27 00:26:43 -0300

    correct

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -15,7 +15,7 @@ function Popup({ onClose }) {
         <div className="popup-overlay">
             <div className="popup-content">
             <h3>THIS IS A WASTE OF TIME</h3>
-            <p>There are 9 levels, which each one constitutes a circle of heaven.<br></br>You'll have to click your way to The Empyrean </p>
+            <p>There are 9 levels, which each one constitutes a circle of heaven.<br></br>You'll have to click your way to The Primum Mobile </p>
                 <div className="button-container">
                     <button onClick={handleClose}>Close</button>
                 </div>
